7e245ead67e9bcee5cd7aa19cb74027104a7d0c7
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2023-03-29 22:42:26 -0600

    Revert "feat: Swerve module class"
    This reverts commit 620fc937ce05e6f9fe8a3f2a9ffe75659b49ea55.

--- robot/src/main/java/org/frc1410/chargedup2023/util/Tuning.java
+++ robot/src/main/java/org/frc1410/chargedup2023/util/Tuning.java
@@ -2,22 +2,4 @@ package org.frc1410.chargedup2023.util;
 
 public interface Tuning {
 
-		// PID values for drive motor
-		double SWERVE_DRIVE_KP = 0;
-		double SWERVE_DRIVE_KI = 0;
-		double SWERVE_DRIVE_KD = 0;
-
-		// PID values for the turning motor
-		double SWERVE_TURNING_KP = 0;
-		double SWERVE_TURNING_KI = 0;
-		double SWERVE_TURNING_KD = 0;
-
-		// Speed, Velocity, and Acceleration values for swerve module
-		double DRIVE_KS = 0;
-		double DRIVE_KV = 0;
-		double DRIVE_KA = 0;
-
-		double TURN_KS = 0;
-		double TURN_KV = 0;
-		double TURN_KA = 0;
 }
