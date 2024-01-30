65bf23cb8289951f4911b6080d0488ec3e80fb85
25thGunter <82180908+25thGunter@users.noreply.github.com>
2024-01-29 17:55:48 -0700

    Assigned colors n some animations. Color names updated. Not as cool as before ngl

--- robot/src/main/java/org/frc1410/crescendo2024/subsystems/LEDs.java
+++ robot/src/main/java/org/frc1410/crescendo2024/subsystems/LEDs.java
@@ -5,10 +5,8 @@ package org.frc1410.crescendo2024.subsystems;
 * */
 
 
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.*;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import com.ctre.phoenix.led.RainbowAnimation;
 
 import static org.frc1410.crescendo2024.util.Constants.*;
 
@@ -16,13 +14,13 @@ public class LEDs implements Subsystem {
 	private CANdle LEDs;
 
 	public enum Colors {
-		BLOOD_OF_ENEMY_DUE_TO_UNFORTUNATE_OCEAN_OIL_SPILL_TURNED_ARSON_FIRE_RED,
-		LIMELIGHT_GREEN,
-		PINK,
-		FIRE_ANIMATION,
-		AUTO_PURPLE,
-		OCEAN_BREEZE_ANIMATION,
-		RAINBOW_ROAD_CRACKHEAD_CLIMBING_TRADEMARK_RAINBOW_ANIMATION,
+		NO_APRIL_TAG_SHOOTING_RED, //Shooting trigger pressed no april tag seen (only speaker april tags)
+		APRIL_TAG_AIM_SHOOTING_GREEN, //Shooting trigger pressed and sees april tag (only speaker april tags)
+		STORED_PINK, //Stored (piece is triggering the intake/storage limit switch)
+		AMP_ARM_FIRE_ANIMATION, //Amp bar out/unfolded
+		AUTO_PURPLE, //Auto
+		IDLE_OCEAN_BREEZE_ANIMATION, //Idle (robot not in a special state) - sea breeze or blue, teal, green, white in like breeze animation
+		CLIMBING_RAINBOW_ANIMATION, //When climbing
 	}
 
 
@@ -31,18 +29,39 @@ public class LEDs implements Subsystem {
 
 		CANdleConfiguration config = new CANdleConfiguration();
 		config.stripType = CANdle.LEDStripType.RGB;
-		config.brightnessScalar = 1.0;
+		config.brightnessScalar = LED_BRIGHTNESS;
+
 		LEDs.configLOSBehavior(true);
 		LEDs.configAllSettings(config);
 
-		LEDs.setLEDs(DEFAULT_LED_COLOR_RGB[0], DEFAULT_LED_COLOR_RGB[1], DEFAULT_LED_COLOR_RGB[2]);
+		defaultLEDsState();
 	}
+
 
 	public void changeLEDsColor(Colors Color) {
 		switch (Color) {
-			case BLOOD_OF_ENEMY_DUE_TO_UNFORTUNATE_OCEAN_OIL_SPILL_TURNED_ARSON_FIRE_RED -> LEDs.setLEDs(255, 0, 0);
-			case LIMELIGHT_GREEN -> LEDs.setLEDs(0, 255, 0);
+			case STORED_PINK -> LEDs.setLEDs(247,2,174);
+			case APRIL_TAG_AIM_SHOOTING_GREEN -> LEDs.setLEDs(10,247,2);
+			case NO_APRIL_TAG_SHOOTING_RED -> LEDs.animate(new StrobeAnimation(255,18,26));
+			case AUTO_PURPLE -> LEDs.setLEDs(146,28,189);
+			case AMP_ARM_FIRE_ANIMATION -> LEDs.animate(new FireAnimation());
+			case CLIMBING_RAINBOW_ANIMATION -> LEDs.animate(new RainbowAnimation());
+			case IDLE_OCEAN_BREEZE_ANIMATION -> LEDs.animate(new ColorFlowAnimation(30, 100, 250));
 		}
+	}
+
+	public void defaultLEDsState() {
+		LEDs.animate(new ColorFlowAnimation(30, 100, 250));
+	}
+
+	private void oceanBreezeAnimation() {
+		/*
+		Colors in order (top down)
+		#1e64fa
+		#05e9f5
+		#05fcba
+		#ffffff
+		 */
 	}
 
 }
