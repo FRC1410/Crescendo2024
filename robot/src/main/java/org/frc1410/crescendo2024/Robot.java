07c6aff7113937930bbf381c12ab275c1b2926e8
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2024-02-02 16:49:22 -0700

    Got rid of the weird cosimo magic

--- robot/src/main/java/org/frc1410/crescendo2024/Robot.java
+++ robot/src/main/java/org/frc1410/crescendo2024/Robot.java
@@ -2,64 +2,12 @@ package org.frc1410.crescendo2024;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import org.frc1410.crescendo2024.util.NetworkTables;
 import org.frc1410.framework.PhaseDrivenRobot;
 
 public final class Robot extends PhaseDrivenRobot {
 
-	//<editor-fold desc="Controllers">
-	//</editor-fold>
-
-	//<editor-fold desc="Auto Selector">
 	private final NetworkTableInstance nt = NetworkTableInstance.getDefault();
 	private final NetworkTable table = nt.getTable("Auto");
-
-	{
-		var layout = """
-		[{
-			"tabName": "Drive",
-			"id": "drive",
-
-			"components": [{
-				"type": "string_select",
-				"title": "Auto Selection",
-				"layout": {
-					"pos": [1, 1],
-					"size": [2, 1]
-				},
-				"topics": ["Auto/Choices", "Auto/Selection"]
-			}, {
-				"type": "clock",
-				"title": "Game Time",
-				"layout": {
-					"pos": [3, 1],
-					"size": [2, 1]
-				},
-				"topics": ["FMSInfo/GameTime"]
-			}, {
-				"type": "node_select",
-				"title": "Selected Node",
-				"layout": {
-					"pos": [5, 1],
-					"size": [1, 1]
-				},
-				"topics": ["Drivetrain/Scoring Pose Index"]
-			}, {
-				"type": "boolean",
-				"title": "LBork Line Break",
-				"layout": {
-					"pos": [6, 1],
-					"size": [1, 1]
-				},
-				"topics": ["LBork/Line Break"]
-			}]
-		}]""";
-
-		// grid, line break, auto, time
-		var pub = NetworkTables.PublisherFactory(nt.getTable("viridian"), "layout", layout);
-	}
-
-	//</editor-fold>
 
 	@Override
 	public void autonomousSequence() {}
